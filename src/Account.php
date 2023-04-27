07d21512b5a8d67bbe49af874501b9a5cfc7e3cf
waliko <wald@lmn.su>
2023-04-27 18:54:36 +0300

    update Account classes

--- src/Account.php
+++ src/Account.php
@@ -17,6 +17,9 @@ class Account
     public $currency;
     public $account_status;
     public $account_type;
+    
+    public $balance_details;
+    public $cards_linked;
 
     public function __construct($base_url,$token,$method) {
         $this->method = $method;
@@ -32,13 +35,52 @@ class Account
             $this->account_status = $data['account_status'];
             $this->account_type = $data['account_type'];
 
+            $this->balance_details = waliko\Yoomoney\Account\BalanceDetails();
+            if (array_key_exists('balance_details',$data)) {
+                if (array_key_exists('available',$data['balance_details']))
+                    $this->balance_details->available = (float)$data['balance_details']['available'];
+
+                if (array_key_exists('blocked',$data['balance_details']))
+                    $this->balance_details->blocked = (float)$data['balance_details']['blocked'];
+
+                if (array_key_exists('debt',$data['balance_details']))
+                    $this->balance_details->debt = (float)$data['balance_details']['debt'];
+
+                if (array_key_exists('deposition_pending',$data['balance_details']))
+                    $this->balance_details->deposition_pending = (float)$data['balance_details']['deposition_pending'];
+
+                if (array_key_exists('total',$data['balance_details']))
+                    $this->balance_details->total = (float)$data['balance_details']['total'];
+
+                if (array_key_exists('hold',$data['balance_details']))
+                    $this->balance_details->hold = (float)$data['balance_details']['hold'];
+
+            }
+
+            $this->cards_linked = array();
+            if (array_key_exists('cards_linked',$data)) {
+                foreach ($data['cards_linked'] as $card_linked)  {
+                    $card = waliko\Yoomoney\Account\Card($card_linked['pan_fragment'], $card_linked['type']);
+                    $this->cards_linked[] = $card;
+                }
+            }
         } else {
             waliko\Yoomoney\Exceptions\InvalidToken();
         }
     }
 
     private function _request() {
+        $access_token = $this->token;
+        $url = $this->base_url.$this->method;
 
+        $headers = array(
+            'Authorization' => 'Bearer '.$access_token,
+            'Content-Type' => 'application/x-www-form-urlencoded'
+        );
+
+        $response = Requests::post($url, $headers);
+
+        return $response->decode_body();
     }
 }
 
